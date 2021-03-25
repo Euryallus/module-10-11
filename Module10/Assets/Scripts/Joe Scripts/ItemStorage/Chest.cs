1ed8d11dde090e06b5ecd85d7f2e6612bdafa3f2
JoeAllen7 <Joseph.Allen@mail.bcu.ac.uk>
2021-03-25 14:25:45 +0000

    Chests with random loot

--- Module10/Assets/Scripts/Joe Scripts/ItemStorage/Chest.cs
+++ Module10/Assets/Scripts/Joe Scripts/ItemStorage/Chest.cs
@@ -3,23 +3,27 @@ using UnityEngine;
 public class Chest : InteractableWithOutline
 {
     [Header("Chest Properties")]
-    [SerializeField] private ChestPanel     chestPanel;
-    [SerializeField] private ItemContainer  itemContainer;
-    [SerializeField] private InventoryPanel playerInventoryPanel;
+    [SerializeField] protected  ChestPanel     chestPanel;
+    [SerializeField] protected  ItemContainer  itemContainer;
+    [SerializeField] protected  InventoryPanel playerInventoryPanel;
 
     public override void Interact()
     {
         //Setup the chest panel so its slot ui elements are linked to the itemcontainer slots for this speficic chest
         itemContainer.LinkSlotsToUI(chestPanel.slotsUI);
 
-        for (int i = 0; i < chestPanel.slotsUI.Count; i++)
-        {
-            chestPanel.slotsUI[i].UpdateUI();
-        }
+        UpdateChestUI();
 
         chestPanel.Show(itemContainer.ContainerId == "linkedChest");
 
         playerInventoryPanel.Show(InventoryShowMode.InventoryOnly, 150.0f);
     }
 
+    protected void UpdateChestUI()
+    {
+        for (int i = 0; i < chestPanel.slotsUI.Count; i++)
+        {
+            chestPanel.slotsUI[i].UpdateUI();
+        }
+    }
 }
--- Module10/Assets/Scripts/Joe Scripts/ItemStorage/LootTable.cs
+++ Module10/Assets/Scripts/Joe Scripts/ItemStorage/LootTable.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Loot Table", menuName = "Loot Table")]
 public class LootTable : ScriptableObject
 {
+    public int                  MinItems    { get { return m_minItems; } }
+    public int                  MaxItems    { get { return m_maxItems; } }
+    public List<WeightedItem>   ItemPool    { get { return m_itemPool; } }
+
     [Space]
     [Header("Loot Table")]
     [Space]
@@ -19,11 +23,26 @@ public class LootTable : ScriptableObject
     [Space]
     [SerializeField] [Tooltip("The items that can be spawned from this loot table, with weights that determine the likelihood of each item spawning")]
     private List<WeightedItem>   m_itemPool;
+
+    public List<Item> GetWeightedItemPool()
+    {
+        List<Item> weightedItemPool = new List<Item>();
+
+        for (int i = 0; i < m_itemPool.Count; i++)
+        {
+            for (int j = 0; j < m_itemPool[i].weight; j++)
+            {
+                weightedItemPool.Add(m_itemPool[i].item);
+            }
+        }
+
+        return weightedItemPool;
+    }
 }
 
 [System.Serializable]
 public struct WeightedItem
 {
     public Item     item;
-    public float    weight;
+    public int    weight;
 }
