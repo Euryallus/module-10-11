41e7febc787a988ad1f981ef46ea4b40bd0c257a
JoeAllen7 <Joseph.Allen@mail.bcu.ac.uk>
2021-04-01 15:04:10 +0100

    Hunger/health progress

--- Module10/Assets/Scripts/Joe Scripts/PlayerStats.cs
+++ Module10/Assets/Scripts/Joe Scripts/PlayerStats.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(PlayerMovement))]
 public class PlayerStats : MonoBehaviour, IPersistentObject
 {
-    [SerializeField] private Slider foodLevelSlider;
+    [Header("Hunger Options (See tooltips for info)")]
 
-    private float foodLevel = 1.0f;
+    [SerializeField] [Tooltip("The number of seconds it will take for the player to starve if they are idle.")]
+    private float  baseTimeToStarve        = 600.0f;
+
+    [SerializeField] [Tooltip("How many times quicker the player's food level will decrease when they are walking/crouching.")]
+    private float  walkHungerMultiplier    = 1.25f;
+
+    [SerializeField] [Tooltip("How many times quicker the player's food level will decrease when they are running.")]
+    private float  runHungerMultiplier     = 1.5f;
+
+    [Header("UI")]
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private Slider foodLevelSlider;
+    [SerializeField] private Image  foodSliderFill;
+
+    private float           health                  = 1.0f;
+    private float           foodLevel               = 1.0f;
+    private PlayerMovement  playerMovementScript;
+    private Animator        foodSliderAnimator;
+
+    private void Awake()
+    {
+        playerMovementScript = GetComponent<PlayerMovement>();
+        foodSliderAnimator = foodSliderFill.gameObject.GetComponent<Animator>();
+    }
 
     protected void Start()
     {
@@ -16,7 +40,28 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
         slm.LoadObjectsSetupEvent       += OnLoadSetup;
         slm.LoadObjectsConfigureEvent   += OnLoadConfigure;
 
+        UpdateHealthUI();
+    }
+
+    private void Update()
+    {
         UpdateFoodLevelUI();
+
+        float foodLevelDecreaseAmount = Time.deltaTime / baseTimeToStarve;
+
+        if (playerMovementScript.PlayerIsMoving())
+        {
+            if(playerMovementScript.GetCurrentMovementState() == PlayerMovement.MovementStates.run)
+            {
+                foodLevelDecreaseAmount *= runHungerMultiplier;
+            }
+            else
+            {
+                foodLevelDecreaseAmount *= walkHungerMultiplier;
+            }
+        }
+
+        DecreaseFoodLevel(foodLevelDecreaseAmount);
     }
 
     private void OnDestroy()
@@ -30,18 +75,25 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
     public void OnSave(SaveData saveData)
     {
         saveData.AddData("playerFoodLevel", foodLevel);
+
+        saveData.AddData("playerHealth", health);
     }
 
     public void OnLoadSetup(SaveData saveData)
     {
         float loadedFoodLevel = saveData.GetData<float>("playerFoodLevel", out bool foodLevelLoadSuccess);
-
         if (foodLevelLoadSuccess)
         {
             foodLevel = loadedFoodLevel;
         }
 
-        UpdateFoodLevelUI();
+        float loadedHealth = saveData.GetData<float>("playerHealth", out bool healthlLoadSuccess);
+        if (healthlLoadSuccess)
+        {
+            health = loadedHealth;
+        }
+
+        UpdateHealthUI();
     }
 
     public void OnLoadConfigure(SaveData saveData)
@@ -53,8 +105,6 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
         foodLevel += amount;
 
         foodLevel = Mathf.Clamp(foodLevel, 0.0f, 1.0f);
-
-        UpdateFoodLevelUI();
     }
 
     public void DecreaseFoodLevel(float amount)
@@ -65,12 +115,24 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
         {
             foodLevel = 0.0f;
         }
-
-        UpdateFoodLevelUI();
     }
 
     private void UpdateFoodLevelUI()
     {
         foodLevelSlider.value = foodLevel;
+
+        if(foodLevel < 0.1f)
+        {
+            foodSliderAnimator.SetBool("Flash", true);
+        }
+        else
+        {
+            foodSliderAnimator.SetBool("Flash", false);
+        }
+    }
+
+    private void UpdateHealthUI()
+    {
+        healthSlider.value = health;
     }
 }
