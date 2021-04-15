9562b666c1d6e961a6b12eeaf7cfc219739a8ad5
Hugo Bailey <lauren.bailey3@mail.bcu.ac.uk>
2021-04-15 20:53:03 +0100

    breath slider

--- Module10/Assets/Scripts/Joe Scripts/Player/PlayerStats.cs
+++ Module10/Assets/Scripts/Joe Scripts/Player/PlayerStats.cs
@@ -29,22 +29,42 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
     [SerializeField] [Tooltip("How frequently (in seconds) the player takes damage when starving.")]
     private float   starveDamageInterval    = 2.0f;
 
+    [SerializeField]
+    private float   drownDamageInterval     = 2.0f;
+
+    [SerializeField]
+    private float   drownDamage               = 0.1f;
+
+    [SerializeField] [Tooltip("Time player takes to drown")]
+    private float baseTimeToDrown           = 60.0f;
+
     [Header("UI")]
     [SerializeField] private Slider healthSlider;       //The slider showing health
     [SerializeField] private Image  healthSliderFill;   //The fill bar for the above slider
     [SerializeField] private Slider foodLevelSlider;    //The slider showing food level
     [SerializeField] private Image  foodSliderFill;     //The fill bar for the above slider
+    [SerializeField] private Slider breathLevelSlider;  //The slider showing breath
+    [SerializeField] private Image breathSliderFill;    //The fill bar for the above slider
+    [SerializeField] private CanvasGroup breathCanvasGroup;
+        
+
 
     #endregion
 
     private float           health    = 1.0f;       //The player's health (0 = death, 1 = full)
     private float           foodLevel = 1.0f;       //The player's food level (0 = starving, 1 = full)
 
+    //Hugo added this blame him if it goes wrong
+    private float           breath    = 1.0f;       //The player's breath level (0 = drowning, 1 = full)
+
     private float           starveDamageTimer;      //Keeps track of seconds passed since damage was taken from starving
+    private float           drownDamageTimer;
     private PlayerMovement  playerMovementScript;   //Reference to the script that controls player movement
     private Animator        healthSliderAnimator;   //Animator used for flashing the health slider bar red when health is low
     private Animator        foodSliderAnimator;     //Animator used for flashing the food slider bar red when food level is low
     private Animator        foodSliderBgAnimator;   //Animator used for flashing the food slider background red when player is starving
+
+    private Animator        breathSliderAnimator;   //Animator used for flashing the breath slider red when player is drowning
 
     private void Awake()
     {
@@ -53,6 +73,8 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
         healthSliderAnimator = healthSliderFill.gameObject.GetComponent<Animator>();
         foodSliderAnimator   = foodSliderFill.gameObject.GetComponent<Animator>();
         foodSliderBgAnimator = foodLevelSlider.transform.Find("Background").GetComponent<Animator>();
+
+        breathSliderAnimator = breathSliderFill.gameObject.GetComponent<Animator>();
     }
 
     protected void Start()
@@ -68,6 +90,7 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
     private void Update()
     {
         float foodLevelDecreaseAmount = Time.deltaTime / baseTimeToStarve;
+        float breathLevelDecreaseAmount = Time.deltaTime / baseTimeToDrown;
 
         if(foodLevel > 0.0f)
         {
@@ -98,8 +121,38 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
             }
         }
 
+        breathCanvasGroup.alpha = breath < 1.0 ? 1 : 0;
+
+        if(breath > 0f)
+        {
+            if(breath < 1.0f)
+            {
+                breathCanvasGroup.alpha = 1f;
+            }
+            if(playerMovementScript.currentMovementState == PlayerMovement.MovementStates.dive)
+            {
+                DecreaseBreath(breathLevelDecreaseAmount);
+            }
+            else
+            {
+                IncreaseBreath(Time.deltaTime);
+            }
+        }
+        else
+        {
+            //player is drowning
+            drownDamageTimer += Time.deltaTime;
+
+            if(drownDamageTimer >= drownDamageInterval)
+            {
+                DecreaseHealth(drownDamage);
+                drownDamageTimer = 0.0f;
+            }
+        }
+
         UpdateHealthUI();
         UpdateFoodLevelUI();
+        UpdateBreathUI();
     }
 
     public void OnSave(SaveData saveData)
@@ -107,6 +160,8 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
         saveData.AddData("playerFoodLevel", foodLevel);
 
         saveData.AddData("playerHealth", health);
+
+        saveData.AddData("playerBreath", breath);
     }
 
     public void OnLoadSetup(SaveData saveData)
@@ -121,6 +176,12 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
         if (healthlLoadSuccess)
         {
             health = loadedHealth;
+        }
+
+        float loadedBreath = saveData.GetData<float>("playerBreath", out bool breathLoadSuccess);
+        if(breathLoadSuccess)
+        {
+            breath = loadedBreath;
         }
     }
 
@@ -160,6 +221,33 @@ public class PlayerStats : MonoBehaviour, IPersistentObject
         {
             health = 0.0f;
         }
+    }
+
+    //Hugo added
+    public void IncreaseBreath(float amount)
+    {
+        breath += amount;
+
+        breath = Mathf.Clamp(breath, 0.0f, 1.0f);
+    }
+
+    public void DecreaseBreath(float amount)
+    {
+        breath -= amount;
+
+        if(breath < 0.0f)
+        {
+            breath = 0.0f;
+        }
+    }
+
+    private void UpdateBreathUI()
+    {
+        breathLevelSlider.value = Mathf.Lerp(breathLevelSlider.value, breath, Time.deltaTime * 25.0f);
+
+        breathSliderAnimator.SetBool("Flash", (breath < 0.15f));
+
+        breathSliderAnimator.SetBool("Flash", (breath == 0.0f));
     }
 
     private void UpdateFoodLevelUI()
