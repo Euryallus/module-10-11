22063780ad4c055bc4eeded8e14c6bb9e678e2f8
Hugo Bailey <lauren.bailey3@mail.bcu.ac.uk>
2021-04-21 20:47:37 +0100

    sounds

---
+++ Module10/Assets/Scripts/Hugo Scripts/Resource collection/crystalHammer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crystalHammer : MonoBehaviour
+{
+    public Crystal rock;
+    public void StopSwinging()
+    {
+        gameObject.GetComponent<Animator>().SetBool("Swing", false);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().StartMoving();
+        GameObject.FindGameObjectWithTag("Hammer").GetComponent<MeshRenderer>().enabled = true;
+        rock.CanBeHit = true;
+    }
+
+    //Added by Joe - plays a sound when the axe is swung, called by an animation event
+    public void SwingEvents()
+    {
+        AudioManager.Instance.PlaySoundEffect3D("whoosh", transform.position);
+    }
+
+    //Added by Joe - shakes the player camera and plays a sound when the axe hits the tree, called by an animation event
+    public void ChopEvents()
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<CameraShake>().ShakeCameraForTime(0.3f, CameraShakeType.ReduceOverTime, 0.03f);
+
+        AudioManager.Instance.PlaySoundEffect3D("crystalHit", transform.position);
+    }
+}
